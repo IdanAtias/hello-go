bd7a5db5b52a4554fb10af8c54ce2446a399ba3f
Idan Atias <idan.atias@silk.us>
2021-02-14 17:19:29 +0200

    add new pkg morestrings with replace rune capability
    Signed-off-by: Idan Atias <idan.atias@silk.us>

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    //"github.com/IdanAtias/hello-go/morestrings" // for fetching this pkg from the remote repo
+    "hello-go/morestrings" // for "local dev"
+)
 
 func main() {
-	fmt.Println("hello go!")
+	fmt.Println(morestrings.ReplaceRunes("hello go!", 'l', '1'))
 }
