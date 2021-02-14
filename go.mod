7e6aa0e2f43e822ccebff2ce4696a504a39c2145
Idan Atias <idan.atias@silk.us>
2021-02-14 18:58:55 +0200

    fix mod name
    Signed-off-by: Idan Atias <idan.atias@silk.us>

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module hello-go
+module github.com/IdanAtias/hello-go
 
 go 1.15
--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
     "fmt"
-    //"github.com/IdanAtias/hello-go/morestrings" // for fetching this pkg from the remote repo
-    "hello-go/morestrings" // for "local dev"
+    "github.com/IdanAtias/hello-go/morestrings"
 )
 
 func main() {
